c73ec20a71074a47b806b525ba5a0a28cb5c1176
Shubham Fuloria <sfuloria10200@gmail.com>
2024-03-09 10:57:13 +0530

    added fire sound

--- Ship/rocket.gd
+++ Ship/rocket.gd
@@ -3,6 +3,7 @@ extends Area2D
 var screen_size;
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$ShootSound.play()
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
